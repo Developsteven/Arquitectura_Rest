2638035755d75c8b244e60a475997e26f28279d0
Developsteven <steven.perez099@gmail.com>
2021-06-25 12:42:01 -0500

    Creando dao IPrograma

---
+++ src/main/java/com/proyec/builsoft/dao/IPrograma.java
@@ -0,0 +1,9 @@
+package com.proyec.builsoft.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.proyec.builsoft.entities.Programa;
+
+public interface IPrograma extends CrudRepository<Programa, Long>{
+
+}
