1825cb74b91edc82826e8051f262f2b20c538cea
Developsteven <steven.perez099@gmail.com>
2021-07-07 10:52:48 -0500

    Creando interface IObservacion

---
+++ src/main/java/com/proyec/builsoft/dao/IObservacion.java
@@ -0,0 +1,9 @@
+package com.proyec.builsoft.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.proyec.builsoft.entities.Observacion;
+
+public interface IObservacion extends CrudRepository<Observacion, Long>{
+
+}
